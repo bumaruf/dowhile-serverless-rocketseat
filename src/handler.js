ca23c3c2cf993ce5519aa2babc72c4b2c30d0789
Otávio Bumaruf <25939563+bumaruf@users.noreply.github.com>
2022-06-22 17:19:50 -0300

    feat: 🎸 creating rest api functions

---
+++ src/handler.js
@@ -0,0 +1,26 @@
+const articleRepository = require('./repository/article')
+
+const createArticle = async event => {
+  const article = JSON.parse(event.body)
+  const result = await articleRepository.create(article)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+  }
+}
+
+const getArticle = async event => {
+  const { id } = event.pathParameters
+  const result = await articleRepository.get(id)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+  }
+}
+
+module.exports = {
+  createArticle,
+  getArticle,
+}
